94d27ed005a0f9d5bbf6b24e62ad4f88522f7997
see1 <mr_see1@vk.com>
2022-07-12 15:33:56 +1000

    add zadacha13

---
+++ homework2/zadacha13/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine().Replace("-", ""));
+int result = number;
+ 
+char[] massivnumber = Convert.ToString(number).ToCharArray();
+int lenghtmassiv = massivnumber.Length - 3;
+ 
+if (massivnumber.Length <= 2) 
+{
+        Console.WriteLine("третьей цифры нет");
+}
+else
+{
+        while(lenghtmassiv != 0) {
+          result /= 10;
+          lenghtmassiv-=1;
+        }
+        result = result % 10;
+        Console.WriteLine($"{result}");
+}
