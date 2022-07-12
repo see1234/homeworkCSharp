01c5b2d5e394708ede1cce87c6ae513a09a98454
see1 <mr_see1@vk.com>
2022-07-12 15:07:18 +1000

    add zadacha6

---
+++ homework1/zadacha6/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number % 2 == 0) {
+    Console.WriteLine("да");
+}
+else {
+    Console.WriteLine("нет");
+}
