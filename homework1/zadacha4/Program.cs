3a41b92c58d8a9b33233a9743c119f4bdd383707
see1 <mr_see1@vk.com>
2022-07-12 14:54:32 +1000

    add zadacha4

---
+++ homework1/zadacha4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число a");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число c");
+int numberC = Convert.ToInt32(Console.ReadLine());
+int max = numberA;
+if(max < numberB) {
+    max = numberB;
+}
+if(max < numberC) {
+    max = numberC;
+}
+Console.WriteLine(max);
