2bf68d0ea3ef3116dcaaf309fe5084249bac1e2a
see1 <mr_see1@vk.com>
2022-07-12 14:50:55 +1000

    add zadacha2

---
+++ homework1/zadacha2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число a");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if(numberA > numberB) {
+    Console.WriteLine("Максимальное число " + numberA);
+    Console.WriteLine("Минимальное число " + numberB);
+}
+else if(numberA == numberB) {
+       Console.WriteLine("Числа одинаковые - " + numberA);
+ 
+}
+else {
+        Console.WriteLine("Максимальное число " + numberB);
+    Console.WriteLine("Минимальное число " + numberA);
+}
