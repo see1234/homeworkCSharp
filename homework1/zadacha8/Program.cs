4ec0b4877b8e318d173cf2174cd6c933c05ffc97
see1 <mr_see1@vk.com>
2022-07-12 15:12:23 +1000

    zadacha8

---
+++ homework1/zadacha8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+while(number != max) {
+    max+=1;
+   if(max % 2 == 0) {
+    Console.Write(max + ",");
+   }
+}
+Console.Write("\b ");
+Console.WriteLine();
