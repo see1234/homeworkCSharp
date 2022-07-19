f56cf43a3cc061f2944e86104ec72e2f140fc611
see1 <mr_see1@vk.com>
2022-07-20 00:37:51 +1000

    upd2

--- homework4/zadacha25/Program.cs
+++ homework4/zadacha25/Program.cs
@@ -2,4 +2,14 @@
 int numberA = Convert.ToInt16(Console.ReadLine());
 Console.WriteLine("Введите число B");
 int numberB = Convert.ToInt16(Console.ReadLine());
-Console.WriteLine("Результат: " + Math.Pow(numberA, numberB));
+Console.WriteLine("Результат: " + pow(numberA, numberB));
+int pow(int number, int pow) {
+    int count = 1;
+    int pows = number;
+    while(pow > count) {
+        count+=1;
+        number=number*pows;
+    }
+    return number;
+}
+  
