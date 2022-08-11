a08c3b367a368c7917c72ae8711eb02ccd949947
see1 <ssaukhin@mail.ru>
2022-08-11 16:45:21 +0300

    add homework#6

---
+++ homework6/Task41/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите количество чисел:");
+int size = Convert.ToInt16(Console.ReadLine());
+int count = 0;
+for (int i = 0; i < size; i++)
+{
+    Console.WriteLine($"Введите №{i + 1} число:");
+    if (Convert.ToInt16(Console.ReadLine()) > 0) count += 1;
+}
+Console.WriteLine("Ответ: " + count);
---
+++ homework6/Task43/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine();
+Console.Write("k1 = ");
+int k1 = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine();
+Console.Write("k2 = ");
+int k2 = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine();
+Console.Write("b1 = ");
+int b1 = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine();
+Console.Write("b2 = ");
+int b2 = Convert.ToInt16(Console.ReadLine());
+
+getResult(k1, k2, b1, b2);
+void getResult(double k1 = 0, double k2 = 0, double b1 = 0, double b2 = 0)
+{
+
+    if (k1 != k2)
+    {
+        double x;
+        x = (b2 - b1) / (k1 - k2);
+        Console.WriteLine($"Точка пересечения: ({Math.Round(x, 2)}; {Math.Round(k1 * x + b1, 2)})");
+    }
+    else if (k1 == k2 && b1 == b2)
+    {
+        Console.WriteLine($"Две прямые полностью равны");
+    }
+    else if (k1 == k2)
+    {
+        Console.WriteLine($"Две прямые не пересекаются");
+    }
+
+}
+
+
+
